4c17769b722da7524a23854b4e05776fb9d406c4
wjj <wjj@wjj-PAF4.(none)>
2014-03-05 19:58:04 +0800

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cards::Application.config.secret_key_base = '272156e01c9b2f00a37a8a703b1bf5145eddb2f42af7e509c6bdce1b84cd740211dc1a75fcf6d93775d29449a5e383c065a146f738423a4da6a52a24e173303a'
